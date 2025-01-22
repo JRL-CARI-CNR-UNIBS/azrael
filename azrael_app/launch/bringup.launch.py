dcca9d89a9849db898c4b5b9d4f97cbd08b7e6dd
RobertoFausti <roberto.fausti@stiima.cnr.it>
2025-01-22 19:06:06 +0100

    new set of launchers

--- azrael_app/launch/bringup.launch.py
+++ azrael_app/launch/bringup.launch.py
@@ -1,24 +1,52 @@
+from launch.conditions import UnlessCondition
 from launch.launch_description import LaunchDescription
-from launch.substitutions import PathJoinSubstitution
-from launch.actions import OpaqueFunction, IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+from launch.actions import OpaqueFunction, IncludeLaunchDescription, DeclareLaunchArgument, GroupAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import PushRosNamespace
 
 def generate_launch_description():
-  return LaunchDescription([OpaqueFunction(function=launch_setup)])
+  launch_args = [
+    DeclareLaunchArgument(name='robot_ip', default_value='192.168.254.31', description='ur net IP'),
+    DeclareLaunchArgument(name='use_fake_hardware', default_value='false', description='use fake hardware'),
+    DeclareLaunchArgument(name='rviz', default_value='true', description='Load Rviz'),
+    DeclareLaunchArgument(name='move_group', default_value='false', description='Start move group'),
+    DeclareLaunchArgument(name='prefix', default_value='azrael', description='ROS Namespace')
+  ]
+
+  return LaunchDescription(launch_args + [OpaqueFunction(function=launch_setup)])
 
 def launch_setup(context):
 
   return [
-    IncludeLaunchDescription(
-      launch_description_source=PythonLaunchDescriptionSource(
-        launch_file_path=PathJoinSubstitution([FindPackageShare('azrael_app'), 'launch', 'moveit.launch.py'])
-      )
-    ),
-    IncludeLaunchDescription(
-      launch_description_source=PythonLaunchDescriptionSource(
-        launch_file_path=PathJoinSubstitution([FindPackageShare('azrael_app'), 'launch', 'controllers.launch.py'])
-      )
-    )
+  GroupAction(
+    actions=[
+      PushRosNamespace(LaunchConfiguration('prefix')),
+      IncludeLaunchDescription(
+        launch_description_source=PythonLaunchDescriptionSource(
+          launch_file_path=PathJoinSubstitution([FindPackageShare('azrael_app'), 'launch', 'moveit.launch.py'])
+        ),
+        launch_arguments=[
+          ('robot_ip', LaunchConfiguration('robot_ip')),
+          ('rviz', LaunchConfiguration('rviz')),
+          ('move_group', LaunchConfiguration('move_group')),
+          ('use_fake_hardware', LaunchConfiguration('use_fake_hardware')),
+          ('prefix', LaunchConfiguration('prefix')),
+        ]
+      ),
+      IncludeLaunchDescription(
+        launch_description_source=PythonLaunchDescriptionSource(
+          launch_file_path=PathJoinSubstitution([FindPackageShare('azrael_app'), 'launch', 'controllers.launch.py'])
+        )
+      ),
+      IncludeLaunchDescription(
+        launch_description_source=PythonLaunchDescriptionSource(
+          launch_file_path=PathJoinSubstitution([FindPackageShare('azrael_app'), 'launch', 'mobile_base.launch.py'])
+        ),
+        condition=UnlessCondition(LaunchConfiguration('use_fake_hardware'))
+      ),
+    ]
+  )
   ]
--- azrael_app/launch/controllers.launch.py
+++ azrael_app/launch/controllers.launch.py
@@ -1,46 +1,55 @@
 from launch.launch_description import LaunchDescription
-from launch.actions import TimerAction
-from launch.substitutions import PathJoinSubstitution
+from launch.actions import TimerAction, OpaqueFunction
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
-
+from launch.actions import DeclareLaunchArgument
 
 def generate_launch_description():
+  launch_args = [
+    DeclareLaunchArgument(name='prefix', default_value='azrael', description='Controller Manager prefix')
+  ]
 
-  ros2_control_config_path = PathJoinSubstitution([FindPackageShare("azrael_app"), "config", "ros2_controllers.yaml"])
+  return LaunchDescription(launch_args + [OpaqueFunction(function=launch_setup)])
+
+
+def launch_setup(context):
+
+  ros2_control_config_path = PathJoinSubstitution([FindPackageShare('azrael_app'), 'config', 'ros2_controllers.yaml'])
 
   controller_manager_node = Node(
-    package="controller_manager",
-    executable="ros2_control_node",
+    package='controller_manager',
+    executable='ros2_control_node',
     parameters=[ros2_control_config_path],
-    # prefix="gnome-terminal -- cgdb -ex run --args",
-    output="screen",
-    remappings=[("/controller_manager/robot_description","/robot_description")],
+    # prefix='gnome-terminal -- cgdb -ex run --args',
+    output='screen',
+    remappings=[('controller_manager/robot_description','robot_description')],
   )
 
   # joint_trajectory_controller_spawner = Node(
-  #   package="controller_manager",
-  #   executable="spawner",
-  #   arguments=["joint_trajectory_controller",
-  #              "--controller-manager", "/controller_manager"],
+  #   package='controller_manager',
+  #   executable='spawner',
+  #   arguments=['joint_trajectory_controller',
+  #              '--controller-manager', 'controller_manager'],
   # )
 
   joint_state_broadcaster_spawner = Node(
-    package="controller_manager",
-    executable="spawner",
-    arguments=["joint_state_broadcaster",
-               "--controller-manager","/controller_manager"],
+    package='controller_manager',
+    executable='spawner',
+    arguments=['joint_state_broadcaster',
+      '--controller-manager', f'{LaunchConfiguration("prefix").perform(context)}/controller_manager'],
   )
 
+  # https://github.com/muttistefano/imm_controller.git
   imm_controller = Node(
-    package="controller_manager",
-    executable="spawner",
-    arguments=["imm_controller",
-              "--controller-manager", "/controller_manager"]
+    package='controller_manager',
+    executable='spawner',
+    arguments=['imm_controller',
+      '--controller-manager', f'{LaunchConfiguration("prefix").perform(context)}/controller_manager'],
   )
 
-  return LaunchDescription([
+  return [
     controller_manager_node,
     TimerAction(
       actions=[joint_state_broadcaster_spawner,
@@ -48,4 +57,4 @@ def generate_launch_description():
               imm_controller],
       period=3.0,
     )
-  ])
+  ]
