5af100a8c964c3b4210eeaf4763338746c33de63
SamueleSandrini <samuele.sandrini@polito.it>
2025-03-27 15:26:06 +0100

    bringup

--- azrael_app/launch/bringup.launch.py
+++ azrael_app/launch/bringup.launch.py
@@ -3,58 +3,53 @@ from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 from launch.actions import OpaqueFunction, IncludeLaunchDescription, DeclareLaunchArgument, GroupAction, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.conditions import IfCondition
+from ament_index_python.packages import get_package_share_directory
 
 
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import PushRosNamespace, Node
 
+import os
+
+
 def generate_launch_description():
   launch_args = [
-    #DeclareLaunchArgument(name='azrael_ip', default_value=None, description='Azrael net IP'),
-    #DeclareLaunchArgument(name='azrael_username', default_value=None, description='Azrael local username'),
-    DeclareLaunchArgument(name='ur_local_ip', default_value='192.168.254.31', description='ur local net IP'),
-    DeclareLaunchArgument(name='use_fake_hardware', default_value='false', description='use fake hardware'),
-    DeclareLaunchArgument(name='rviz', default_value='true', description='Load Rviz'),
-    DeclareLaunchArgument(name='move_group', default_value='false', description='Start move group'),
-    DeclareLaunchArgument(name='prefix', default_value='azrael', description='ROS Namespace')
+    DeclareLaunchArgument(name='launch_ur', default_value='true', description='Launch ur robot launcher'),
+    DeclareLaunchArgument(name='robot_ip', default_value='192.168.254.100', description='ur local net IP'),
   ]
 
   return LaunchDescription(launch_args + [OpaqueFunction(function=launch_setup)])
 
 def launch_setup(context):
+    azrael_dir = get_package_share_directory('azrael_app')
+    launch_dir = os.path.join(azrael_dir, 'launch')
+    on_robot_dir = os.path.join(azrael_dir, 'launch/on_robot')
 
-  #remote_launch_command = "bash -i -c 'ros2 launch azrael_app on_robot.launch.py'"
+    launch_ur = LaunchConfiguration('launch_ur').perform(context)
+    robot_ip = LaunchConfiguration('robot_ip').perform(context)
 
-  return [
-  GroupAction(
-    actions=[
-      PushRosNamespace(LaunchConfiguration('prefix')),
-      # # IMM Controller
-      # IncludeLaunchDescription(
-      #   launch_description_source=PythonLaunchDescriptionSource(
-      #     launch_file_path=PathJoinSubstitution([FindPackageShare('azrael_app'), 'launch', 'spawn_controllers.launch.py'])
-      #   )
-      # ),
-      # # Cartesian Controller
-      IncludeLaunchDescription(
-        launch_description_source=PythonLaunchDescriptionSource(
-          launch_file_path=PathJoinSubstitution([FindPackageShare('azrael_app'), 'launch', 'spawn_cartesian_controllers.launch.py'])
-        )
-      ),
-      Node(
-        package='rviz2',
-        executable='rviz2',
-        parameters=[
-          #moveit_config.to_dict()
-        ],
-        arguments=['-d', PathJoinSubstitution([FindPackageShare('azrael_app'), 'rviz', 'setup.rviz'])],
-        condition=IfCondition(LaunchConfiguration('rviz')),
-      )
-      # ExecuteProcess(
-      #   cmd=['ssh', '-t', f'{LaunchConfiguration("azrael_username").perform(context)}@{LaunchConfiguration("azrael_ip").perform(context)}',
-      #     f'{remote_launch_command}'],
-      #   output='both'
-      # )
-    ]
-  )
-  ]
+    bringup_cmd_group = GroupAction(
+        [
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(launch_dir, 'ur_bringup.launch.py')
+                ),
+                condition=IfCondition(launch_ur),
+                launch_arguments={
+                    'fake_ur': 'false',
+                    'headless_mode': 'true',
+                    'robot_ip': robot_ip,
+                }.items(),
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(on_robot_dir, 'on_robot.launch.py')
+                ),
+            ),
+        ]
+    )
+    ld = LaunchDescription()
+    ld.add_action(bringup_cmd_group)
+    
+    return ld
+
