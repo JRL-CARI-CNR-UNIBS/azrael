c249973d7a273fd2fd154f411b5aaf4791c4f5f7
SamueleSandrini <samuele.sandrini@polito.it>
2025-03-27 15:42:20 +0100

    bringup

--- azrael_app/launch/bringup.launch.py
+++ azrael_app/launch/bringup.launch.py
@@ -52,18 +52,18 @@ def generate_launch_description():
 
     bringup_cmd_group = GroupAction(
         [
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    os.path.join(launch_dir, 'ur_bringup.launch.py')
-                ),
-                condition=IfCondition(launch_ur),
-                launch_arguments={
-                    'fake_ur': 'false',
-                    'headless_mode': 'true',
-                    'robot_ip': robot_ip,
-                    'launch_rviz': 'false',
-                }.items(),
-            ),
+            # IncludeLaunchDescription(
+            #     PythonLaunchDescriptionSource(
+            #         os.path.join(launch_dir, 'ur_bringup.launch.py')
+            #     ),
+            #     condition=IfCondition(launch_ur),
+            #     launch_arguments={
+            #         'fake_ur': 'false',
+            #         'headless_mode': 'true',
+            #         'robot_ip': robot_ip,
+            #         'launch_rviz': 'false',
+            #     }.items(),
+            # ),
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(
                     os.path.join(on_robot_dir, 'on_robot.launch.py')
