f5881d6b8529f51b6801ac6ed12cfae3a57284e3
SamueleSandrini <samuele.sandrini@polito.it>
2025-03-27 15:32:20 +0100

    bringup

--- azrael_app/launch/bringup.launch.py
+++ azrael_app/launch/bringup.launch.py
@@ -39,21 +39,17 @@ from launch_ros.actions import PushRosNamespace, Node
 import os
 
 
-def generate_launch_description():
-  launch_args = [
-    DeclareLaunchArgument(name='launch_ur', default_value='true', description='Launch ur robot launcher'),
-    DeclareLaunchArgument(name='robot_ip', default_value='192.168.254.100', description='ur local net IP'),
-  ]
-
-  return LaunchDescription(launch_args + [OpaqueFunction(function=launch_setup)])
-
 def launch_setup(context):
     azrael_dir = get_package_share_directory('azrael_app')
     launch_dir = os.path.join(azrael_dir, 'launch')
     on_robot_dir = os.path.join(azrael_dir, 'launch/on_robot')
 
-    launch_ur = LaunchConfiguration('launch_ur').perform(context)
-    robot_ip = LaunchConfiguration('robot_ip').perform(context)
+    launch_ur_cmd = DeclareLaunchArgument(name='launch_ur', default_value='true', description='Launch ur robot launcher'),
+    robot_ip_cmd = DeclareLaunchArgument(name='robot_ip', default_value='192.168.254.100', description='ur local net IP'),
+
+    
+    launch_ur = LaunchConfiguration('launch_ur')
+    robot_ip = LaunchConfiguration('robot_ip')
 
     bringup_cmd_group = GroupAction(
         [
@@ -78,5 +74,7 @@ def launch_setup(context):
     )
     ld = LaunchDescription()
     ld.add_action(bringup_cmd_group)
+    ld.add_action(launch_ur_cmd)
+    ld.add_action(robot_ip_cmd)
     
     return ld
