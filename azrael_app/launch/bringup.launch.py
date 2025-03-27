b411b40804d583d4ae5bee3a4f2ccb20c8791323
SamueleSandrini <samuele.sandrini@polito.it>
2025-03-27 16:02:17 +0100

    bringup

--- azrael_app/launch/bringup.launch.py
+++ azrael_app/launch/bringup.launch.py
@@ -58,10 +58,10 @@ def generate_launch_description():
                 ),
                 condition=IfCondition(launch_ur),
                 launch_arguments={
+                    'launch_rviz': 'false',
                     'fake_ur': 'false',
                     'headless_mode': 'true',
-                    'robot_ip': robot_ip,
-                    'launch_rviz': 'false',
+                    'robot_ip': robot_ip
                 }.items(),
             ),
             # IncludeLaunchDescription(
