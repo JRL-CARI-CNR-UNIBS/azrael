e6d270c42b80fdcfd5ede3c0d3b4790511e06d34
SamueleSandrini <samuele.sandrini@polito.it>
2025-03-27 15:28:48 +0100

    bringup

--- azrael_app/launch/bringup.launch.py
+++ azrael_app/launch/bringup.launch.py
@@ -25,6 +25,33 @@ def launch_setup(context):
     launch_dir = os.path.join(azrael_dir, 'launch')
     on_robot_dir = os.path.join(azrael_dir, 'launch/on_robot')
 
+from launch.launch_description import LaunchDescription
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+from launch.actions import OpaqueFunction, IncludeLaunchDescription, DeclareLaunchArgument, GroupAction, ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import PushRosNamespace, Node
+
+import os
+
+
+def generate_launch_description():
+  launch_args = [
+    DeclareLaunchArgument(name='launch_ur', default_value='true', description='Launch ur robot launcher'),
+    DeclareLaunchArgument(name='robot_ip', default_value='192.168.254.100', description='ur local net IP'),
+  ]
+
+  return LaunchDescription(launch_args + [OpaqueFunction(function=launch_setup)])
+
+def launch_setup(context):
+    azrael_dir = get_package_share_directory('azrael_app')
+    launch_dir = os.path.join(azrael_dir, 'launch')
+    on_robot_dir = os.path.join(azrael_dir, 'launch/on_robot')
+
     launch_ur = LaunchConfiguration('launch_ur').perform(context)
     robot_ip = LaunchConfiguration('robot_ip').perform(context)
 
@@ -39,6 +66,7 @@ def launch_setup(context):
                     'fake_ur': 'false',
                     'headless_mode': 'true',
                     'robot_ip': robot_ip,
+                    'launch_rviz': 'false',
                 }.items(),
             ),
             IncludeLaunchDescription(
@@ -52,4 +80,3 @@ def launch_setup(context):
     ld.add_action(bringup_cmd_group)
     
     return ld
-
