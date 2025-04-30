287071970e0406e1b44be637f48ef73502375939
SamueleSandrini <samuele.sandrini@polito.it>
2025-04-30 15:02:28 +0200

    force_torque_sensor_broadcaster

--- azrael_app/launch/ur_bringup.launch.py
+++ azrael_app/launch/ur_bringup.launch.py
@@ -248,7 +248,7 @@ def launch_setup(context, *args, **kwargs):
         'joint_state_broadcaster',
         # 'io_and_status_controller',
         # 'speed_scaling_state_broadcaster',
-        # 'force_torque_sensor_broadcaster',
+        'force_torque_sensor_broadcaster',
         # 'ur_configuration_controller',
         # 'robotiq_activation_controller',
     ]
