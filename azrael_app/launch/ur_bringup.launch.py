d4e513f53cd54c97ba703902dd13e8843d21b17d
SamueleSandrini <samuele.sandrini@polito.it>
2025-09-04 18:37:20 +0200

    Gripper controllers

--- azrael_app/launch/ur_bringup.launch.py
+++ azrael_app/launch/ur_bringup.launch.py
@@ -249,7 +249,7 @@ def launch_setup(context, *args, **kwargs):
         'joint_state_broadcaster',
         # 'io_and_status_controller',
         # 'speed_scaling_state_broadcaster',
-        'gripper_action_controller'
+        'gripper_action_controller',
         'force_torque_sensor_broadcaster',
         # 'ur_configuration_controller',
         # 'robotiq_activation_controller',
