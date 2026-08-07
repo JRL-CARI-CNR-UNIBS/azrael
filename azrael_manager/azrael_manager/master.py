5e9b0fa56041056d9b53389f6ee0fb8dc94f9f7a
Roberto Fausti <robertofausti@cnr.it>
2026-08-07 15:23:17 +0200

    add kill service

--- azrael_manager/azrael_manager/master.py
+++ azrael_manager/azrael_manager/master.py
@@ -1,9 +1,10 @@
 import rclpy
 import rclpy.logging
 from rclpy.node import Node
+from rclpy.duration import Duration
 
 from azrael_manager.interface import Interface
-from azrael_manager_msgs.srv import InvokeService, GetParameterList
+from azrael_manager_msgs.srv import InvokeService, GetParameterList, KillService
 
 import subprocess
 import importlib
@@ -17,21 +18,29 @@ class AzraelMasterNode(Node):
     def __init__(self, fconfig):
         super().__init__("__azrael_manager__")
 
-        self.invoke_launcher = self.create_service(srv_type=InvokeService, srv_name="/azrael/system/invoke_service", callback=self.handle_service)
+        self.invoke_launcher = self.create_service(srv_type=InvokeService, srv_name="/azrael/system/invoke_service", callback=self.handle_service_callback)
         self.list_param_server = self.create_service(srv_type=GetParameterList, srv_name="/azrael/system/get_parameters_for_service", callback=self.list_parameters_callback)
+        self.kill_server = self.create_service(srv_type=KillService, srv_name="/azrael/system/kill_service", callback=self.kill_service_callback)
         self._defined_modules_and_classes: dict[str, list[str]] = {}
         self._available_services: dict[str, Interface] = {}
         self._procs: dict[str, subprocess.Popen] = {}
 
         self.setup(fconfig)
 
-    def handle_service(self, req: InvokeService.Request, res: InvokeService.Response):
-        if req.service not in self._available_services:
-            message = f'Service requested [{req.service}] is not available'
+    def is_service_available(self, name: str) -> tuple[Interface | None, int, str]:
+        if name not in self._available_services:
+            message = f'Service requested [{name}] is not available'
             self.get_logger().error(message)
-            res.error_code = InvokeService.Response.FAILED
-            res.error_message = message
-        selected = self._available_services[req.service]
+            error_code = InvokeService.Response.FAILED
+            error_message = message
+            return (None, error_code, error_message)
+        return (self._available_services[name], InvokeService.Response.SUCCESS, "No problem")
+
+
+    def handle_service_callback(self, req: InvokeService.Request, res: InvokeService.Response):
+        selected, res.error_code, res.error_message = self.is_service_available(req.service)
+        if selected == None:
+            return res
         self.get_logger().debug(req.parameters)
         res.error_code, res.error_message = selected.validate_parameters(req.parameters)
         self._procs[req.service] = selected.run()
@@ -74,14 +83,31 @@ class AzraelMasterNode(Node):
 
         return True
 
+
     def list_parameters_callback(self, req: GetParameterList.Request, res: GetParameterList.Response):
-        if req.service not in self._available_services:
-            message = f'Service requested [{req.service}] is not available'
-            self.get_logger().error(message)
-            res.error_code = InvokeService.Response.FAILED
-            res.error_message = message
-        selected = self._available_services[req.service]
+        selected, res.error_code, res.error_message = self.is_service_available(req.service)
+        if selected == None:
+            return res
         res.parameter_names, res.parameter_types, res.parameter_descriptions, res.error_code, res.error_message = selected.list_parameters()
+        return res
+
+    def kill_service_callback(self, req: KillService.Request, res: KillService.Response):
+        selected, res.error_code, res.error_message = self.is_service_available(req.service)
+        if selected == None:
+            return res
+        if req.service in self._procs:
+            if req.signal == KillService.Request.KILL:
+                self._procs[req.service].kill()
+            else:
+                self._procs[req.service].terminate()
+            self.get_clock().sleep_for(Duration(seconds=1))
+            ret = self._procs[req.service].poll()
+            if ret and ret < 0:
+                self._procs.pop(req.service)
+            else:
+                res.error_message = f"Service {req.service} still running"
+        else:
+            res.error_message = f"No process linked to service {req.service}"
         return res
 
 
